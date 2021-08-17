dbd33a6a1ad165ab001e5d81829f350025a43258
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-08-17 23:00:09 +0530

    [akhila]Refactor:changes in css of experience page and dlc page

--- Js/gallery.js
+++ Js/gallery.js
@@ -1,1 +1,9 @@
-
+// document.getElementById("dropbtn").onclick = function(){
+//     myfunction()
+// }
+// function myfunction(){
+//     document.getElementById("header").classList.toggle("show");
+// }
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
