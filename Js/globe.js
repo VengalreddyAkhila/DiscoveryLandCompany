9e35753f7acac6cb6d9fb1b12ff1ecf37b8af890
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-08-19 15:13:48 +0530

    [akhila]Refactor:added the section sto press page

--- Js/globe.js
+++ Js/globe.js
@@ -1,28 +1,43 @@
-var blackBoxContents = [
+var blackbox= [
     {
         image : "../Assests/mainpage/Maddison.jpg",
         maintext : "Madison",
-        subtext : "La Quinta , California"
+        subtext : "La Quinta , California",
+        weather : 230,
+        windspeed : "10.3mph",
+        suntemp : 44
     },
     {
         image : "../Assests/mainpage/BakersBay.jpg",
         maintext : "Baker's Bay",
-        subtext : "Great Guana Cay, Bahamas"
+        subtext : "Great Guana Cay, Bahamas",
+        weather:100,
+        windspeed : "22.5mph",
+        suntemp : 45
     },
     {
         image : "../Assests/mainpage/mountaintop.jpg",
         maintext : "Mountaintop",
-        subtext : "Cashiers, North Carolina"
+        subtext : "Cashiers, North Carolina",
+        weather : 250,
+        windspeed :"33.0mph",
+        suntemp : 22
     },
     {
         image : "../Assests/mainpage/Mirabel-.jpeg",
         maintext : "Mirabel",
-        subtext : "Scottsdale,Arizona"
+        subtext : "Scottsdale,Arizona",
+        weather : 300,
+        windspeed: "15.5mph",
+        suntemp : 35
     },
     {
         image : "../Assests/mainpage/Vaquero.jpg",
         maintext : "Vaquero",
-        subtext : "West Lake,Texas"
+        subtext : "West Lake,Texas",
+        weather : 320,
+        windspeed : "12.2mph",
+        suntemp : 38
     }
 ]
 
@@ -38,10 +53,13 @@ function prev(){
 }
 
 function changeBlackBox(){
-    if(index < 0) index = blackBoxContents.length -1;
-    if(index > blackBoxContents.length ) index = 0 ;
+    if(index < 0) index = blackbox.length -1;
+    if(index > blackbox.length ) index = 0 ;
 
-    document.getElementById("black-box-img").src = blackBoxContents[index].image;
-    document.getElementById("main-text").innerHTML = blackBoxContents[index].maintext;
-    document.getElementById("sub-text").innerHTML = blackBoxContents[index].subtext;
+    document.getElementById("black-box-img").src = blackbox[index].image;
+    document.getElementById("main-text").innerHTML = blackbox[index].maintext;
+    document.getElementById("sub-text").innerHTML = blackbox[index].subtext;
+    document.getElementById("weather").innerHTML = blackbox[index].weather;
+    document.getElementById("windspeed").innerHTML = blackbox[index].windspeed;
+    document.getElementById("suntemp").innerHTML = blackbox[index].suntemp;
 }
