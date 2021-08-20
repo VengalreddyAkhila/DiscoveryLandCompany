253934f030d18fef5ab6c44b2e27b24f6635b3cc
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-08-20 22:13:11 +0530

    [akhila]Refactor:make changes in carousel part in mainpage

--- Js/globe.js
+++ Js/globe.js
@@ -1,38 +1,33 @@
 var blackbox= [
-    {
-        image : "../Assests/mainpage/Maddison.jpg",
+    {        
         maintext : "Madison",
         subtext : "La Quinta , California",
         weather : 230,
         windspeed : "10.3mph",
         suntemp : 44
     },
-    {
-        image : "../Assests/mainpage/BakersBay.jpg",
+    {       
         maintext : "Baker's Bay",
         subtext : "Great Guana Cay, Bahamas",
         weather:100,
         windspeed : "22.5mph",
         suntemp : 45
     },
-    {
-        image : "../Assests/mainpage/mountaintop.jpg",
+    {       
         maintext : "Mountaintop",
         subtext : "Cashiers, North Carolina",
         weather : 250,
         windspeed :"33.0mph",
         suntemp : 22
     },
-    {
-        image : "../Assests/mainpage/Mirabel-.jpeg",
+    {        
         maintext : "Mirabel",
         subtext : "Scottsdale,Arizona",
         weather : 300,
         windspeed: "15.5mph",
         suntemp : 35
     },
-    {
-        image : "../Assests/mainpage/Vaquero.jpg",
+    {        
         maintext : "Vaquero",
         subtext : "West Lake,Texas",
         weather : 320,
@@ -40,23 +35,19 @@ var blackbox= [
         suntemp : 38
     }
 ]
-
 var index = 0;
 function next(){
     index ++ ;
     changeBlackBox();
 }
-
 function prev(){
     index --;
     changeBlackBox();
 }
-
 function changeBlackBox(){
     if(index < 0) index = blackbox.length -1;
     if(index > blackbox.length ) index = 0 ;
-
-    document.getElementById("black-box-img").src = blackbox[index].image;
+    
     document.getElementById("main-text").innerHTML = blackbox[index].maintext;
     document.getElementById("sub-text").innerHTML = blackbox[index].subtext;
     document.getElementById("weather").innerHTML = blackbox[index].weather;
--- Js/press.js
+++ Js/press.js
@@ -1,24 +1,9 @@
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  // var y = document.getElementsByClassName("img-text");
-  if (n > x.length) {slideIndex = 1}
-  // if(n > y.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  // if(n < 1) {slideIndex = y.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  // for(i = 0; i < y.length; i++){
-  //   y[i].style.display = "none";
-  // }
-  x[slideIndex-1].style.display = "block";
-  // y[slideIndex-1].style.display = "block";
-}
+ 
+   let a = document.getElementById('leftbutton');
+   a.addEventListener('click',function ( ) {
+        console.log("hello");
+   })
+   let b = document.getElementById('rightbutton');
+   b.addEventListener('click',function ( ) {
+       console.log("hi");
+   })
